822485f9b9ce8f6341dc3dfae2e8b629f4d57f3c
cadolphe <christopher.adolphe@gmail.com>
2022-07-30 09:06:52 +0400

    Added associations between User and Product models

--- course-project/app.js
+++ course-project/app.js
@@ -9,6 +9,9 @@ const shopRouter = require('./routes/shop-router');
 const adminRouter = require('./routes/admin-router');
 const errorRouter = require('./routes/error-router');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
@@ -19,6 +22,30 @@ app.use(shopRouter);
 app.use('/admin', adminRouter);
 app.use(errorRouter);
 
+/**
+ * Registering a middleware function to store the user
+ * model in the request so that it is accessible everywhere
+ * in the Node.js app
+*/
+app.use(async (request, response, next) => {
+  try {
+    const user = await User.findByPk(1);
+
+    request.user = user;
+
+    next();
+  } catch (error) {
+    console.log(`Sorry, an error occurred when fetching user: ${error.message}`);
+  }
+});
+
+/**
+ * Using assosications methods of the models
+ * to add a foreign key constraint to the
+ * attributes
+*/
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
 
 /**
  * Using the `sync()` method from the sequelize
@@ -29,6 +56,19 @@ app.use(errorRouter);
 sequelize
   .sync()
   .then(result => {
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({
+        name: 'Christopher',
+        email: 'christopher@onlineshop.com',
+      });
+    }
+
+    return user;
+  })
+  .then(user => {
     app.listen(port, () => {
       console.log(`Server listening... http://localhost:${port}`);
     });
