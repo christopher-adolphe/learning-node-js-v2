363585fb01d69f8ca5df579d05d10c726308ae95
cadolphe <christopher.adolphe@gmail.com>
2023-02-21 13:03:11 +0400

    WIP: Connecting React application to the REST API

--- rest-api-course-project/server/controllers/feed-controller.js
+++ rest-api-course-project/server/controllers/feed-controller.js
@@ -1,7 +1,34 @@
 let posts = [
-  { id: 1, title: 'The First Post', content: 'This is the first post content.' },
-  { id: 2, title: 'The Second Post', content: 'This is the second post content.' },
-  { id: 3, title: 'The Third Post', content: 'This is the third post content.' },
+  {
+    _id: '1',
+    title: 'The First Post',
+    content: 'This is the first post content.',
+    imageUrl: 'images/react-logo.png',
+    creator: {
+      name: 'Jane Doe',
+    },
+    createdAt: new Date(),
+  },
+  {
+    _id: '2',
+    title: 'The Second Post',
+    content: 'This is the second post content.',
+    imageUrl: 'images/react-logo.png',
+    creator: {
+      name: 'Greg Lutzska',
+    },
+    createdAt: new Date(),
+  },
+  {
+    _id: '3',
+    title: 'The Third Post',
+    content: 'This is the third post content.',
+    imageUrl: 'images/react-logo.png',
+    creator: {
+      name: 'Rachel Robins',
+    },
+    createdAt: new Date(),
+  },
 ];
 
 const getPosts = (request, response, next) => {
@@ -15,7 +42,7 @@ const getPosts = (request, response, next) => {
 const getPost = (request, response, next) => {
   const { id } = request.params;
 
-  const post = posts.find(post => post.id === +id);
+  const post = posts.find(post => post._id === id);
 
   response.status(200).json({ post });
 };
@@ -24,9 +51,14 @@ const createPost = (request, response, next) => {
   const { title, content } = request.body;
 
   const post = {
-    id: posts.length + 1,
+    _id: `${posts.length + 1}`,
     title,
     content,
+    imageUrl: 'images/react-logo.png',
+    creator: {
+      name: 'Rachel Robins',
+    },
+    createdAt: new Date(),
   };
 
   posts.push(post);
@@ -40,8 +72,8 @@ const createPost = (request, response, next) => {
 const deletePost = (request, response, next) => {
   const { id } = request.params;
 
-  const post = posts.find(post => post.id === +id);
-  posts = posts.filter(post => post.id !== +id);
+  const post = posts.find(post => post._id === id);
+  posts = posts.filter(post => post._id !== id);
 
   response.status(200).json({
     message: `Post with id: ${id} successfully deleted`,
