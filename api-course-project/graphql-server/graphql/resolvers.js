7acc3133da3480c24ed4ff966d2521c62217ad09
cadolphe <christopher.adolphe@gmail.com>
2023-03-02 23:55:17 +0400

    Installed validator for graphql

--- api-course-project/graphql-server/graphql/resolvers.js
+++ api-course-project/graphql-server/graphql/resolvers.js
@@ -1,12 +1,28 @@
 const bcrypt = require('bcryptjs');
+const validator = require('validator');
 
 const User = require('../models/user');
 
 module.exports = {
   createUser: async function({ userInputData }, req) {
     const { name, email, password } = userInputData;
+    const errors = [];
+
+    if (!validator.isEmail(email)) {
+      errors.push({ message: 'Please enter a valid email!' });
+    }
+
+    if (!validator.isEmpty(password) || !validator.isLength(password, { min: 5 })) {
+      errors.push({ message: 'Please enter a password of minimum 5 charactors!' });
+    }
 
     try {
+      if (error.isLength > 0) {
+        const error = new Error(('Invalid input'));
+
+        throw error;
+      }
+      
       const existingUser = await User.findOne({ email: email });
 
       if (existingUser) {
