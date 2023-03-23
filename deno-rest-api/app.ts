84db8a0fcd1874508b7967305b2c2237e538df55
cadolphe <christopher.adolphe@gmail.com>
2023-03-24 01:38:37 +0400

    Updated deno REST API with mongodb

--- deno-rest-api/app.ts
+++ deno-rest-api/app.ts
@@ -1,11 +1,22 @@
 import { Application } from 'https://deno.land/x/oak@v11.1.0/mod.ts';
+// import { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts;
+
+import { connect } from './db/db_client.ts';
 
 import todosRoutes from './routes/todos.ts';
 
+connect();
+
 const app = new Application();
 
+// Enabling CORS using oakCors
+// app.use(oakCors());
+
+// Enabling CORS by manually setting the proper headers
 app.use(async (context, next) => {
-  console.log('Some async middleware');
+  context.response.headers.set('Access-Control-Allow-Origin', '*');
+  context.response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  context.response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
 
   await next();
 });
@@ -13,4 +24,4 @@ app.use(async (context, next) => {
 app.use(todosRoutes.routes());
 app.use(todosRoutes.allowedMethods());
 
-await app.listen({ port: 3000 });
+await app.listen({ port: 8000 });
--- deno-rest-api/routes/todos.ts
+++ deno-rest-api/routes/todos.ts
@@ -1,58 +1,101 @@
 import { Router } from 'https://deno.land/x/oak@v11.1.0/mod.ts';
+import { Bson, ObjectId } from 'https://deno.land/x/mongo@v0.31.1/mod.ts';
+
+import { getDB } from '../db/db_client.ts';
 
 const router = new Router({
   prefix: '/todos',
 });
 
+interface TodoSchema {
+  _id: ObjectId;
+  text: string;
+}
+
 interface Todo {
-  id: string;
+  id?: string;
   text: string;
 };
 
 let todos: Todo[] = [];
 
-router.get('/', (context) => {
-  context.response.body = { todos };
+router.get('/', async (context) => {
+  const todos: TodoSchema[]  = await getDB().collection<TodoSchema>('todos').find().toArray();
+  const transformedTodos = todos.map(todo => ({ id: todo._id.toString(), text: todo.text }));
+
+  context.response.body = { todos: transformedTodos };
 });
 
 router.post('/', async (context) => {
   const data = await context.request.body();
   const { text } = await data.value;
 
+  // const newTodo: Todo = {
+  //   id: new Date().toISOString(),
+  //   text,
+  // };
+
+  // todos.push(newTodo);
+
   const newTodo: Todo = {
-    id: new Date().toISOString(),
     text,
   };
 
-  todos.push(newTodo);
+  const newTodoId = await getDB().collection('todos').insertOne(newTodo);
+
+  newTodo.id = newTodoId.$oid;
 
   context.response.body = { message: 'New Todo successfully added', todos };
 });
 
 router.put('/:id', async (context) => {
   const { id } = context.params;
-  const todoIndex = todos.findIndex(todo => todo.id === id);
+  // const todoIndex = todos.findIndex(todo => todo.id === id);
 
-  if (todoIndex >= 0) {
+  // if (todoIndex >= 0) {
+  //   const data = await context.request.body();
+  //   const { text } = await data.value;
+
+  //   todos[todoIndex] = { ...todos[todoIndex], text };
+
+  //   return context.response.body = { message: `Todo with id: ${id} successfully updated`, todos };
+  // }
+
+  const todo = await getDB().collection('todos').findOne({ _id: new Bson.ObjectId(id) });
+
+  if (todo) {
     const data = await context.request.body();
     const { text } = await data.value;
 
-    todos[todoIndex] = { ...todos[todoIndex], text };
+    const result = await getDB().collection('todos').updateOne(
+      { _id: new Bson.ObjectId(id) },
+      { $set: { text } }
+    );
 
-    return context.response.body = { message: `Todo with id: ${id} successfully updated`, todos };
+    console.log('updated todo: ', result);
+
+    return context.response.body = { message: `Todo with id: ${id} successfully updated`, todo: result };
   }
 
   context.response.body = { message: `Sorry, no Todo with id: ${id} found` };
 });
 
-router.delete('/:id', (context) => {
+router.delete('/:id', async (context) => {
   const { id } = context.params;
-  const todoIndex = todos.findIndex(todo => todo.id === id);
+  // const todoIndex = todos.findIndex(todo => todo.id === id);
 
-  if (todoIndex >= 0) {
-    todos = todos.filter(todo => todo.id !== id);
+  // if (todoIndex >= 0) {
+  //   todos = todos.filter(todo => todo.id !== id);
 
-    return context.response.body = { message: `Todo with id: ${id} successfully deleted`, todos };
+  //   return context.response.body = { message: `Todo with id: ${id} successfully deleted`, todos };
+  // }
+
+  const todo = await getDB().collection('todos').findOne({ _id: new Bson.ObjectId(id) });
+
+  if (todo) {
+    await getDB().collection('todos').deleteOne({ _id: new Bson.ObjectId(id) });
+
+    return context.response.body = { message: `Todo with id: ${id} successfully deleted`, todo };
   }
 
   context.response.body = { message: `Sorry, no Todo with id: ${id} found` };
